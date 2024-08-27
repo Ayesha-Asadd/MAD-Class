f840462d8b74c984e18987f3fb830b109b8cb5dd
Ayesha Mukhtar Asad <168007496+Ayesha-Asadd@users.noreply.github.com>
2024-08-27 13:55:28 +0500

    Updated

---
+++ main.js
@@ -0,0 +1,29 @@
+var first_name= 'Ayesha'
+function get_name()
+{
+ var last_name="global";
+ console.log(first_name);
+ console.log(last_name)
+}
+get_name();
+
+var greet = () => 
+    {
+        console.log("hello!");
+        let a=2;
+        if (a==2){
+            let a=3;
+            console.log(a);
+        }
+
+
+
+    }
+    
+    greet();
+    const arr= function (){
+        console.log("Annonymous ftn");
+    }
+
+
+
